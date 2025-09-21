059b5f7fce50cc020fc7454ed2a32889a5a4a188
Mart510 <martynandrewbillings@gmail.com>
2025-09-21 17:29:51 +0100

    more turbo tweaks

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+const isRunningOnGitHubActions = !!process.env.GITHUB_ACTIONS
+
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -8,7 +10,9 @@ export default defineConfig({
       enabled: true,
       provider: 'playwright',
       instances: [
-        { browser: 'chromium' },
+        { browser: 'chromium',
+          headless: isRunningOnGitHubActions
+        },
       ],
     }
   }
