64125cf5b8dcd6bd84df00dbabb0abbd2d0e990a
Mart510 <martynandrewbillings@gmail.com>
2025-09-21 16:00:25 +0100

    added dev switch for hono server to run in non-headless when local

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,26 @@
 import { Hono } from 'hono';
-import { serve } from '@hono/node-server';
 import { trpcServer } from '@hono/trpc-server';
-import { appRouter } from './router'; // Your tRPC router
-
+import { appRouter } from './router'; 
+import { serve } from '@hono/node-server';
 
 // Create Hono app
 const app = new Hono();
 
-// Forward /trpc/* routes to tRPC using hono-trpc adapter
-    app.use(
-      '/api/*', // The URL path for your tRPC API
-      trpcServer({ router: appRouter })
-    );
 
-// Start server
-// serve({
-//   fetch: app.fetch,
-//   port: 4000,
-// });
+app.use(
+    '/api/*', // The URL path for your tRPC API
+    trpcServer({ router: appRouter })
+  );
+
+const isLocal = process.env.NODE_ENV !== 'production';
+
+if (isLocal) {
+  serve({
+    fetch: app.fetch,
+    port: 4000,
+  });
+  console.log('🚀 Hono server running at http://localhost:4000/api');
+}
 
 export default app;
 
-console.log('🚀 Hono server running at http://localhost:4000/trpc');
