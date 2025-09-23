ea14d3e068d282917dbae2edb28b87a586c77e01
Mart510 <martynandrewbillings@gmail.com>
2025-09-23 20:18:48 +0100

    env

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -2,21 +2,12 @@
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../../server/src/router';
 
-let baseURL: string;
-
-// Decide URL based on environment
-if (import.meta.env.MODE === 'development') {
-  // Local dev backend
-  baseURL = 'http://localhost:4000/api';
-} else {
-  // Production (Vercel)
-  baseURL = '';
-}
+const baseUrl = import.meta.env.VITE_TRPC_URL;
 
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: baseURL,
+      url: baseUrl,
     }),
   ],
 });
