488dca4ddbf1adc8d5a47516b3b4fb208ca51ab1
Mart510 <martynandrewbillings@gmail.com>
2025-09-23 20:07:20 +0100

    test fix

--- client/src/App.browser.test.tsx
+++ client/src/App.browser.test.tsx
@@ -1,10 +1,11 @@
 import { describe, it, expect } from 'vitest'
 import {render} from  'vitest-browser-react'        
 import App from './App'
+import { trpc } from './trpc'
 
 describe('App component', () => {
   it('renders the h1 with "Vite + React"', () => {
-    const { container } = render(<App />)
+    const { container } = render(<App trpc={trpc} />)
     const heading = container.querySelector('h1')
     expect(heading?.textContent).toBe('Vite + React')
   })
